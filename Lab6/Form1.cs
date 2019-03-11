5013bdc0c1e205bab0e4ea25a5ac4e914d8a7715
inkjett <mail22sms@gmail.com>
2019-03-11 16:57:03 +0500

    чет начал делать

--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -12,9 +12,29 @@ namespace Lab6
 {
     public partial class Form1 : Form
     {
+        Int32 x = 10, y = 10;
         public Form1()
         {
             InitializeComponent();
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics G = this.CreateGraphics();
+            Pen P1 = new Pen(Color.Red, 3);
+            G.DrawRectangle(P1, 70, 50, 150, 100);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
         }
     }
 }
