0a71caf127144b2afb9ec4d260cc15f851dc3219
inkjett <mail22sms@gmail.com>
2019-03-11 22:23:46 +0500

    последняя версия из дома

--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -12,7 +12,9 @@ namespace Lab6
 {
     public partial class Form1 : Form
     {
-        Int32 x = 10, y = 10;
+        Color[] col = { Color.Red, Color.Green,Color.Gray,Color.Gold};
+        //Int32 x = 250, y = 250;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,19 +23,18 @@ namespace Lab6
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics G = this.CreateGraphics();
-            Pen P1 = new Pen(Color.Red, 3);
-            G.DrawRectangle(P1, 70, 50, 150, 100);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-   
+            //G.FillEllipse(new SolidBrush(col[1]), x, y, 40, 40);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Graphics G = pictureBox1.CreateGraphics();
+            for (int x = 30; x<150;x++ )
+            {
+                pictureBox1.Refresh();
+                G.FillEllipse(new SolidBrush(col[1]), x, 40, 40, 40);
 
-
+            }
 
         }
     }
