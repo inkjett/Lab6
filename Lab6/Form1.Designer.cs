ee2d866f2bdc6879369dad116dc0c68600d9b0ba
inkjett <mail22sms@gmail.com>
2019-03-12 17:21:49 +0500

    сделал

--- Lab6/Form1.Designer.cs
+++ Lab6/Form1.Designer.cs
@@ -30,16 +30,29 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(241, 340);
+            this.button1.Location = new System.Drawing.Point(212, 340);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(148, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Запусить круг";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -51,11 +64,136 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(192, 485);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Высчитать формулу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 376);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "-1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(52, 419);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(212, 376);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "-2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(212, 419);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "3";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(377, 376);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "0,5";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(377, 419);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            this.textBox6.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Xmin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Xmax";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(176, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Ymin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Ymax";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(341, 383);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "hx";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(342, 426);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "hy";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 445);
+            this.ClientSize = new System.Drawing.Size(592, 564);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -63,6 +201,7 @@
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +209,19 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Lab6/Form1.cs
+++ Lab6/Form1.cs
@@ -7,35 +7,180 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Lab6
 {
     public partial class Form1 : Form
     {
-        Color[] col = { Color.Red, Color.Green,Color.Gray,Color.Gold};
-        //Int32 x = 250, y = 250;
-
+        public void method_elips_run_triangl(ref PictureBox pictureBox) // метод движения круга
+        {
+            Graphics G = pictureBox.CreateGraphics();
+            Color[] col = { Color.Red, Color.Green, Color.Gray, Color.Gold };
+            Random rnd = new Random();
+            
+            Color color_in = col[rnd.Next(1,4)];
+            for (int i = 0; i < 240; i++)
+            {
+                pictureBox.Refresh();
+                G.FillEllipse(new SolidBrush(color_in), 280 - i, 260 - i, 40, 40);
+                Thread.Sleep(1);
+            }
+            color_in = col[rnd.Next(1, 4)];
+            for (int i = 0; i < 500; i++)
+            {
+                pictureBox.Refresh();
+                G.FillEllipse(new SolidBrush(color_in), 40 + i, 20, 40, 40);
+                Thread.Sleep(1);
+            }
+            color_in = col[rnd.Next(1, 4)];
+            for (int i = 0; i < 240; i++)
+            {
+                pictureBox.Refresh();
+                G.FillEllipse(new SolidBrush(color_in), 540 - i, 20 + i, 40, 40);
+                Thread.Sleep(1);
+            }                                 
+        }
+                          
         public Form1()
         {
             InitializeComponent();
-
         }
+
+
+        Double xmin = -1, xmax = 2, ymin = -1, ymax = 1, hx, hy, mx, my;
+        int ax1 = 50, ax2 = 50, ay1 = 0, ay2 = 0, d, c, sx=3, sy=3;
+        Pen pn = new Pen(Color.Red, 3); // перо для графика
+        Pen pn1 = new Pen(Color.Green, 3); // перо для сетки
+        Pen pn2 = new Pen(Color.Black, 3); // перо для осей
+        Font fn = new Font("Arial", 12);// шрифт для осей
+        Font fn1 = new Font("Arial", 11);// шфрифт для засечек 
+
+        private Double f(double x)// функция, граифк которой рисуется
+        {
+            return 7 * ((x * Math.Tan(x)) / 2);
+        }
+
+        private void Extrem() // расчет масштаба и смещения
+        {
+            mx = (d - ax1) / (ymax - xmin);//Масштаб по оси X
+            my = (c - ay1) / (ymin - ymax);//Масштаю по оси Y
+            sx = ax1 - Convert.ToInt32(mx * xmin);
+            sy = ay1 - Convert.ToInt32(my * ymax);
+        }
+
+
+
+        private void grid_x(Graphics F)
+        {
+            double x;
+            int i, n;
+            x = xmin;
+            n = Convert.ToInt32((xmax - xmin) / hx) + 1;
+            for (i = 0; i <= n; i++)
+            {
+                F.DrawLine(pn2, Convert.ToInt32(x * mx) + sx,
+                Convert.ToInt32(ymin * my) + sy,
+                Convert.ToInt32(x * mx) + sx,
+                Convert.ToInt32(ymax * my) + sy);
+                x += hx;
+            }
+        }
+
+        private void grid_y(Graphics F)
+        {
+            double y;
+            int i, n;
+            y = ymin;
+            n = Convert.ToInt32((ymax - ymin) / hy) + 1;
+            for (i = 1; i <= n; i++)
+            {
+                F.DrawLine(pn1, Convert.ToInt32(xmin * mx) + sx,
+                Convert.ToInt32(y * my) + sy,
+                Convert.ToInt32(xmax * mx) + sx,
+                Convert.ToInt32(y * my) + sy);
+                y += hy;
+            }                       
+        }
+
+        private void axis_x(Graphics Graph)
+        {
+            Graph.DrawLine(pn2, Convert.ToInt32(xmin * mx) + sx, sy,
+            Convert.ToInt32(xmax * mx) + sx, sy);
+            Graph.DrawString("", fn, Brushes.Black,
+            Convert.ToInt32(xmax * mx) + sx + 20, sy);
+        }
+
+        private void axis_y(Graphics Graph)
+        {
+            Graph.DrawLine(pn2, sx, Convert.ToInt32(ymin * my) + sy,
+            sx, Convert.ToInt32(ymax * my) + sy);
+            Graph.DrawString("Y", fn, Brushes.Black, sx - 20,
+            Convert.ToInt32(ymax * my) + sy);
+        }
+
+
+        private void z_x(Graphics F)
+        {
+            for (double i = xmin; i <= xmax; i += hx)
+            {
+                F.DrawLine(pn2, Convert.ToInt32(i * mx) + sx,
+                sy, Convert.ToInt32(i * mx) + sx, 5 + sy);
+                F.DrawString(Convert.ToString(i), fn1,
+                Brushes.Black, Convert.ToInt32(i * mx) + sx - 60, 2 + sy);
+            }
+        }
+
+        private void z_y(Graphics F)
+        {
+            for (double i = ymin; i <= ymax; i += hy)
+            {
+                F.DrawLine(pn2, sx,
+                Convert.ToInt32(i * my) + sy, 5 + sx,
+                Convert.ToInt32(i * my) + sy);
+                if (i != 0) F.DrawString(Convert.ToString(i),
+                fn1, Brushes.Black, 3 + sx,
+                Convert.ToInt32(i * my) + sy);
+            }
+        }
+
+               
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics G = this.CreateGraphics();
-            //G.FillEllipse(new SolidBrush(col[1]), x, y, 40, 40);
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Graphics G = pictureBox1.CreateGraphics();
-            for (int x = 30; x<150;x++ )
-            {
-                pictureBox1.Refresh();
-                G.FillEllipse(new SolidBrush(col[1]), x, 40, 40, 40);
+            method_elips_run_triangl(ref pictureBox1);
+        }
 
-            }
-
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics F = pictureBox1.CreateGraphics();
+            double h;
+            pictureBox1.Refresh();
+            ax1 = 30; ay1 = 20; ax2 = 30; ay2 = 15;
+            c = pictureBox1.Size.Height - ay1 - ay2;
+            d = pictureBox1.Size.Width - ax1 - ax2;
+            xmin = Convert.ToDouble(textBox1.Text);
+            xmax = Convert.ToDouble(textBox2.Text);
+            ymin = Convert.ToDouble(textBox3.Text);
+            ymax = Convert.ToDouble(textBox4.Text);
+            hx = Convert.ToDouble(textBox5.Text);
+            hy = Convert.ToDouble(textBox6.Text);
+            Extrem();
+            grid_x(F);
+            grid_y(F);
+            axis_x(F);
+            axis_y(F);
+            z_x(F); z_y(F);
+            h = 0.001;
+            for (double x = -1; x <= 1; x += h)
+                F.DrawLine(pn, Convert.ToInt32(x * mx) + sx,
+                Convert.ToInt32(f(x) * my) + sy,
+                Convert.ToInt32((x + h) * mx) + sx,
+                Convert.ToInt32(f(x + h) * my) + sy);
         }
     }
 }
